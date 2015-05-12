9db43c386b21f368f831cd5610e6c2000b15f0eb
Brandon Nguyen <eagleeye2555@gmail.com>
2015-05-12 11:04:40 -0700

    Milestone 4

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define.
-define('client_ID', 'c73d173254d844b89d8117954f97d9ee');
-define('client_Secret', '971766cd8c4f4af7b7a6ff36f32b68b0');
-define('redirectURI', 'http://localhost/appapi/index.php');
+define('clientID', 'dd86d0663cfe4bda9c8bd5ffda8d7a76');
+define('clientSecret', '44a1cb4b5f694a879bc71cf8e8238ae1');
+define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 ?>
 
@@ -20,7 +20,7 @@ define('ImageDirectory', 'pics/');
 		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account 
 		After getting approval, we are now going to have the information so that we can play with it.
 		-->
-		<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
+		<a href="https:api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">LOGIN</a>
 		<script src="js/main.js"></script>
 	</body>
 </html>
