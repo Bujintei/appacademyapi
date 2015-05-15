0ab50a5c4b02ada5dcec7c3f6988eacbc374c852
Brandon Nguyen <eagleeye2555@gmail.com>
2015-05-15 05:36:13 -0700

    Milestone 11

--- index.php
+++ index.php
@@ -31,6 +31,18 @@ function getUserID($userName){
 	echo $results['data']['0']['id'];
 }
 
+//function to print out images onto screen
+function printImages($userID){
+	$url = 'http://api.instagram.com/v1/users/' . $userID . '/media/recent?client_id=' . clientID . '&count=5';
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+	//Parse through the info one by one
+	foreach($results['data'] as $items){
+		$image_url = $items['images']['low_resolution']['url'];
+		echo '<img src =" '. $image_url .' "/><br/>';
+	}
+}
+
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
@@ -51,7 +63,13 @@ $result = curl_exec($curl);
 curl_close($curl);
 
 $results = json_decode($result, true);
-getUserID($results['user']['username']);
+
+$userName = $results['user']['username'];
+
+$userID = getUserID($userName);
+
+printImages($userID);
+
 }
 else{
 ?>
