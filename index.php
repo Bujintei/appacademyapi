4c16805323f6cf7d01d5720a9000007ff9ab05c1
Brandon Nguyen <eagleeye2555@gmail.com>
2015-05-15 05:24:39 -0700

    Milestone 9 & 10

--- index.php
+++ index.php
@@ -10,6 +10,27 @@ define('clientSecret', '44a1cb4b5f694a879bc71cf8e8238ae1');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 
+//function that connects to Instagram
+function connectToInstagram($url){
+	$ch = curl_init();
+	curl_setopt_array($ch, array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SSL_VERIFYHOST => 2,
+		));
+	$result= curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
+//function to get userID
+function getUserID($userName){
+	$url = 'http://api.instagram.com/v1/users/search?q='.$userName.'&client_id='.clientID;
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+	echo $results['data']['0']['id'];
+}
+
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
@@ -30,10 +51,9 @@ $result = curl_exec($curl);
 curl_close($curl);
 
 $results = json_decode($result, true);
-echo $results['user']['username'];
+getUserID($results['user']['username']);
 }
-else{}
-
+else{
 ?>
 
 <!DOCTYPE html>
@@ -49,3 +69,6 @@ else{}
 		<script src="js/main.js"></script>
 	</body>
 </html>
+<?php
+}
+?>
